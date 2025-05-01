1afb85bfdb748423a036d587b7367587fdb5a799
Eoloth <ruben.msalles@gmail.com>
2025-05-01 13:10:31 -0400

    corregido nombre ruta imgen MC

--- src/pages/eventos/EventoMileyCyrus.jsx
+++ src/pages/eventos/EventoMileyCyrus.jsx
@@ -18,7 +18,7 @@ const EventoMileyCyrus = () => {
   return (
     <EventoBase
       titulo="Concierto de Miley Cyrus en Chile"
-      imagen="/images/miley_cyrus.jpg"
+      imagen="/images/miley_cirus.jpg"
       alt="Miley Cyrus en Chile"
       parrafos={parrafos}
     >
